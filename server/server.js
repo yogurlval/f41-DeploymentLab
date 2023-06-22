0fda61756ccfd64c9e7ee2def5c4a4ec853bdc97
Valentina Bowman <yogurlval@gmail.com>
2023-06-22 12:42:38 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+
+
+
+
+app.listen(4000, console.log(`Server runnin on 4000!`))
